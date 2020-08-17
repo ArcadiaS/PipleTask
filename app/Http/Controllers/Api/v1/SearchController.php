7a36489b4e472b85b0bd95c88fc0e6e0652e9ade
Kağan KAHRAMAN <kaane.kahramane@gmail.com>
2020-08-17 22:39:45 +0300

    elasticquent package added

---
+++ app/Http/Controllers/Api/v1/SearchController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProfileResource;
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $query = [
+                    'match' => [
+                        'title' => 'consequatur'
+                    ]
+        ];
+        $profiles = Profile::searchByQuery(array('match' => array('title' => 'consequatur')));
+
+        return response()->json(ProfileResource::collection($profiles));
+        
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
